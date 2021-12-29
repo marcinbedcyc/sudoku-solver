0b6d3b51a2255c8cb8ecad9d5e96424f09b76c8f
Marcin Cyc <cmartindev@gmail.com>
2021-12-29 17:32:11 +0100

    Add simple sudoku solver module with unit tests for module

---
+++ sudoku_solver/solver.py
@@ -0,0 +1,210 @@
+class InvalidSudokuString(Exception):
+    pass
+
+
+class InvalidSudokuGrid(Exception):
+    pass
+
+
+def is_valid_column(grid, column_number):
+    """
+    Checks whether column is correct sudoku column. Column is valid when each
+    value is different (zeros are omitted).
+
+    Args:
+        grid (list): Matrix with partially/fully filled-in sudoku.
+        column_number (int): The column number.
+
+    Returns:
+        bool: Whether column is valid sudoku column or not.
+    """
+    column = list(filter(
+        lambda value: value != 0, [row[column_number] for row in grid]
+    ))
+    print(column, column_number, set(column
+                                     ))
+    return len(column) == len(set(column))
+
+
+def is_valid_row(grid, row_num):
+    """
+    Checks whether row is correct sudoku row. Row is valid when each value is
+    different (zeros are omitted).
+
+    Args:
+        grid (list): Matrix with partially/fully filled-in sudoku.
+        row_num (int): The row number.
+
+    Returns:
+        bool: Whether row is valid sudoku row or not.
+    """
+    row = list(filter(lambda value: value != 0, grid[row_num]))
+    return len(row) == len(set(row))
+
+
+def is_valid_boxes(grid):
+    """
+    Checks whether all boxes are correct sudoku box. Box is valid when each
+    value is different (zeros are omitted).
+
+    Args:
+        grid (list): Matrix with partially/fully filled-in sudoku.
+
+    Returns:
+        bool: Whether all boxes are valid sudoku box or not.
+    """
+    for row in range(0, 9, 3):
+        for col in range(0, 9, 3):
+            box = []
+            for x in range(row, row+3):
+                for y in range(col, col+3):
+                    if grid[x][y] != 0:
+                        box.append(grid[x][y])
+            if len(box) != len(set(box)):
+                return False
+    return True
+
+
+def is_valid_sudoku_grid(grid):
+    """
+    Checks whether grid is correct sudoku grid. Grid is correct when each row
+    is correct sudoku row, each column is correct sudoku column and each box is
+    correct sudoku box.
+
+    Args:
+        grid (list): Matrix with partially/fully filled-in sudoku.
+
+    Returns:
+        bool: Whether passed grid is valid sudoku grid or not.
+    """
+    for i in range(9):
+        is_valid = is_valid_row(grid, i)
+        if not is_valid:
+            return False
+        is_valid = is_valid_column(grid, i)
+        if not is_valid:
+            return False
+    return is_valid_boxes(grid)
+
+
+def sudoku_str_to_grid(sudoku_str):
+    """Convert sudoku string to sudoku grid (2D list).
+
+    Args:
+        sudoku_str (str): Sudoku grid as string which can only contains digits
+            or dots(same as 0).
+
+    Raises:
+        InvalidSudokuString: String is too long or contains disallowed
+            characters.
+    """
+    if len(sudoku_str) != 81:
+        raise InvalidSudokuString('Too many characters!')
+
+    # Characters validation
+    for letter in sudoku_str:
+        if not letter.isdecimal() and letter != '.':
+            raise InvalidSudokuString(f'{letter} is not a decimal or "." !')
+
+    sudoku_str = sudoku_str.replace('.', '0')
+    grid = [[] for _ in range(9)]
+    for i, letter in enumerate(sudoku_str):
+        grid[i // 9].append(int(letter))
+    return grid
+
+
+def grid_to_sudoku_str(grid):
+    """Convert sudoku grid (2D list) to sudoku string.
+
+    Args:
+        grid (list): 2D list  (9x9) as representation of sudoku grid.
+
+    Raises:
+        InvalidSudokuGrid: When grid has got wrong dimension.
+
+    Returns:
+        str: Sudoku grid as string which can only contains digits or dots (same
+            as 0).
+    """
+    if len(grid) != 9:
+        raise InvalidSudokuGrid('Wrong dimension of sudoku matrix!')
+    for row in grid:
+        if len(row) != 9:
+            raise InvalidSudokuGrid('Wrong dimension of sudoku matrix!')
+    return ''.join(
+        str(digit) if digit != 0 else '.' for row in grid for digit in row
+    )
+
+
+def is_safe(grid, row, column, number):
+    """
+    Checks whether it will be legal to assign number to the given row & column.
+
+    Args:
+        grid (list): Matrix with partially filled-in sudoku.
+        row (int): Row index.
+        column (int): Column index.
+        number (int): Assignation value.
+
+    Returns:
+        bool: Whether assignation is legal or not.
+    """
+    # Checking row
+    for x in range(9):
+        if grid[row][x] == number:
+            return False
+
+    # Checking column
+    for x in range(9):
+        if grid[x][column] == number:
+            return False
+
+    # Checking box
+    start_row = row - row % 3
+    start_column = column - column % 3
+    for x in range(3):
+        for y in range(3):
+            if grid[x + start_row][y + start_column] == number:
+                return False
+    return True
+
+
+def solve_sudoku(grid, row=0, column=0):
+    """
+    Takes a partially filled-in grid and attempt to assign values to all
+    unassigned locations in such a way to meet the requirements for Sudoku
+    solution (non-duplication across rows, columns, and boxes)
+
+    Args:
+            grid (list): Partially filled-in sudoku grid.
+            row (int): Row index. Defaults to 0.
+            column (int): Column index. Defaults to 0.
+
+    Returns:
+            bool: If sudoku can be solved or not. Solution in `grid` if exists.
+    """
+    # Avoid further backtracking
+    if (row == 8 and column == 9):
+        return True
+
+    # Go to next row of grid
+    if column == 9:
+        row += 1
+        column = 0
+
+    # Skip cell if it contains value (go to next step)
+    if grid[row][column] > 0:
+        return solve_sudoku(grid, row, column + 1)
+
+    for number in range(1, 10):
+        # Check if `number` can be assigned in `row` & `column`. Move to next
+        # column if `number` can be inserted.
+        if is_safe(grid, row, column, number):
+            grid[row][column] = number
+            if solve_sudoku(grid, row, column + 1):
+                return True
+
+        # Assumption went wrong, reset value and go to next assumption with
+        # different `number` value
+        grid[row][column] = 0
+    return False
---
+++ tests/sudoku_solver/test_solver.py
@@ -0,0 +1,120 @@
+import pytest
+from contextlib import contextmanager
+
+from sudoku_solver.solver import (
+    is_valid_column,
+    is_valid_row,
+    is_valid_boxes,
+    is_valid_sudoku_grid,
+    grid_to_sudoku_str,
+    sudoku_str_to_grid,
+    is_safe,
+    solve_sudoku,
+    InvalidSudokuString,
+    InvalidSudokuGrid
+)
+from tests.fixtures_and_data.sudoku_grids import (
+    ones, zeros, ones_3x9, ones_9x5, valid_rows, valid_cols, valid_boxes,
+    grid1, grid2, grid2_result, difficult_case_grid, difficult_case_grid_result
+)
+
+
+@contextmanager
+def does_not_raise():
+    yield
+
+
+@pytest.mark.parametrize('grid, result', [
+    (ones, False),
+    (zeros, True),
+    (valid_rows, False),
+    (valid_cols, True),
+    (valid_boxes, False)
+])
+@pytest.mark.parametrize('col', [i for i in range(9)])
+def test_is_valid_column(grid, col, result):
+    assert is_valid_column(grid, col) is result
+
+
+@pytest.mark.parametrize('grid, result', [
+    (ones, False),
+    (zeros, True),
+    (valid_rows, True),
+    (valid_cols, False),
+    (valid_boxes, False)
+])
+@pytest.mark.parametrize('row', [i for i in range(9)])
+def test_is_valid_row(grid, row, result):
+    assert is_valid_row(grid, row) is result
+
+
+@pytest.mark.parametrize('grid, result', [
+    (ones, False),
+    (zeros, True),
+    (valid_rows, False),
+    (valid_cols, False),
+    (valid_boxes, True)
+])
+def test_is_valid_boxes(grid, result):
+    assert is_valid_boxes(grid) is result
+
+
+@pytest.mark.parametrize('grid, result', [
+    (ones, False),
+    (zeros, True),
+    (valid_rows, False),
+    (valid_cols, False),
+    (valid_boxes, False)
+])
+def test_is_valid_sudoku_grid(grid, result):
+    assert is_valid_sudoku_grid(grid) is result
+
+
+@pytest.mark.parametrize('grid, sudoku_str, expectation', [
+    (None, "1"*15, pytest.raises(InvalidSudokuString)),
+    (zeros, "."*81, does_not_raise()),
+    (zeros, "0"*81, does_not_raise()),
+    (None, "zaq"*27, pytest.raises(InvalidSudokuString)),
+    (valid_rows, "123456789"*9, does_not_raise()),
+    (valid_boxes, "123123123456456456789789789"*3, does_not_raise()),
+])
+def test_sudoku_str_to_grid(grid, sudoku_str, expectation):
+    with expectation:
+        assert sudoku_str_to_grid(sudoku_str) == grid
+
+
+@pytest.mark.parametrize('grid, sudoku_str, expectation', [
+    (ones, "1"*81, does_not_raise()),
+    (zeros, "."*81, does_not_raise()),
+    (ones_3x9, "1"*27, pytest.raises(InvalidSudokuGrid)),
+    (ones_9x5, "1"*45, pytest.raises(InvalidSudokuGrid)),
+    (valid_rows, "123456789"*9, does_not_raise()),
+    (valid_boxes, "123123123456456456789789789"*3, does_not_raise()),
+])
+def test_grid_to_sudoku_str(grid, sudoku_str, expectation):
+    with expectation:
+        assert grid_to_sudoku_str(grid) == sudoku_str
+
+
+@pytest.mark.parametrize('grid, x, y, val, result', [
+    (grid1, 0, 2, 3, False),
+    (grid1, 2, 0, 7, True),
+    (grid1, 2, 1, 7, True),
+    (grid1, 2, 2, 7, False),
+    (grid1, 0, 2, 4, True),
+    (grid1, 0, 2, 4, True),
+    (grid1, 1, 2, 4, True),
+    (grid1, 2, 2, 4, False),
+])
+def test_is_safe(grid, x, y, val, result):
+    assert is_safe(grid, x, y, val) == result
+
+
+@pytest.mark.parametrize('sudoku_grid, result_grid, is_solved', [
+    (grid2, grid2_result, True),
+    (difficult_case_grid, difficult_case_grid_result, True)
+])
+def test_solving_sudoku_workflow(sudoku_grid, result_grid, is_solved):
+    result = sudoku_grid.copy()
+    assert solve_sudoku(result) == is_solved
+    assert result == result_grid
